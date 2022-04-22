9782892d3a57cc1e1f19f8ffac571bf5378f1bae
EdD-Nuke <75727942+EdD-Nuke@users.noreply.github.com>
2022-04-22 11:11:05 +0200

    Updated with distribute func

--- LBS.py
+++ LBS.py
@@ -11,20 +11,20 @@ import pandas as pd
 protocollayers = {}
 protocolAttributes = {}
 
-global TCP_list
+#global TCP_list
 TCP_list = [] #TCP list packet storage
-global UDP_list
+#global UDP_list
 UDP_list = [] #UDP list package storage
-global SYN_counter
+#global SYN_counter
 SYN_counter = 0
-global ACK_counter
+#global ACK_counter
 ACK_counter = 0
 
 
 
 def Cap():
     timer_analysis_start = time.time
-    capture = pyshark.LiveCapture("WI-FI")
+    capture = pyshark.LiveCapture("WI-FI", display_filter='tcp.analysis.fast_retransmission')
 
     for packet in capture:
         timer_analysis_end = time.time
@@ -48,23 +48,70 @@ def counters_up_to_date(packet) :
 
 
 def distribute(packet) :
-    TCP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/TCP.txt", "w")
-    UDP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP.txt", "w")
+    TCP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/TCP.txt", "a")
+    UDP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP.txt", "a")
+    UDP_DNS_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP_DNS.txt", "a")
     try:
+        #counts_seq += 1
         protocol = packet.transport_layer
-        if "tcp" in packet:
-            TCP_object.writelines("TCP")
-            print("TCP")
+
+        #Check if TCP syn and fin messages are fine then scan trough the tcp and fetch to file
+        field_names = packet.tcp._all_fields
+        field_values = packet.tcp._all_fields.values()
+        for field_name in field_names:
+            for field_value in field_values:
+                if field_name == 'tcp.payload':
+                    print(f'{field_name} -- {field_value[100:120, 1]}') #100:120, 1
+                    print("*"*10 + "Continuiing" + "*"*10)
+
+        if "tcp" in packet and "ip" not in packet:
+            #packet_time = packet.sniff_time
+            str_N = ('Name: ', packet.layers) #[-2]
+            str_S_p = ('Scr. Port: ', packet[protocol].srcport)
+            str_D_p = ('Dst. Port: ', packet[protocol].dstport)
+            str1 = (str_N, str_S_p, str_D_p )# "Seq.": packet.sequence, "Len": packet.length, "Time": time.time, "Attri": TLP_attr   packet_time
+            TCP_object.writelines(str(str1) + os.linesep)
+            print ('%s  %s:%s --> %s:%s' % (str_N, packet[protocol].srcaddr, str_S_p, str_D_p, packet[protocol].dstaddr))
             TCP_list.extend(packet)
         elif "udp" in packet:
-            UDP_object.writelines("UDP")
-            print("UDP")
+            packet_time2 = packet.sniff_time
+            str_N_2 = ('Name: ', packet.layers[-2])
+            str_S_p_2 = ('Scr. Port: ', packet[protocol].srcport)
+            str_D_p_2 = ('Dst. Port: ', packet[protocol].dstport)
+            str2 = (str_N_2, str_S_p_2, str_D_p_2, packet_time2)
+            #{"Name":packet.layers, "Src. Port": packet[protocol].srcport, "Dst. Port": packet[protocol].dstport, "Len": packet.length, "Time": time.time}
+            UDP_object.writelines(str(str2) + os.linesep)
             UDP_list.extend(packet)
+            print ('%s  %s:%s --> %s:%s' % (str_N_2, packet[protocol].srcaddr, str_S_p, str_D_p, packet[protocol].dstaddr))
+        
+        # if "tcp" in packet or "udp" in packet :
+        #     TLP_packets += [[packet.layers, packet[protocol].srcport, packet[protocol].dstport, packet.length, time.time, TLP_attr]]
+        #     TCP_attr += 1
+        # elif "icmp" in packet :
+        #     ICMP_packets += [[packet.layers, packet[protocol].srcport, packet[protocol].dst, packet.length, time.time, ICMP_attr]]
+        #     ICMP_attr += 1
+
+        elif "udp" in packet and packet[packet.transport_layer].dstport == '53':
+            packet_time2 = packet.sniff_time
+            str3 = ('Name: ', packet.layers)
+            str3 += ('Scr. Port: ', packet[protocol].srcport)
+            str3 += ('Dst. Port: ', packet[protocol].dstport)
+            UDP_DNS_object.writelines(str(str3) + os.linesep)
+            UDP_list_DNS.extend(packet)
+        #
+        #
+        #Here look for more packet petterns!
+
+        #CHECKER FOR SEQ DATA
+        seq = packet[protocol].seq
+        print("packet count seq is %s " %(seq)) #counts_seq
+
     except AttributeError as e:
             print(e)
             SystemExit()
     TCP_object.close()
     UDP_object.close()
+    UDP_DNS_object.close()
 
 DOS_list = []
 Attcak_list2 = []
