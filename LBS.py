5e4fc5f30293932875b880ea624e23784d411e41
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2022-04-22 22:33:06 +0200

    Implemented SYN flood check and UDP check (not fully done)

--- LBS.py
+++ LBS.py
@@ -15,42 +15,56 @@ protocolAttributes = {}
 TCP_list = [] #TCP list packet storage
 #global UDP_list
 UDP_list = [] #UDP list package storage
-#global SYN_counter
 SYN_counter = 0
-#global ACK_counter
 ACK_counter = 0
+UDP_counter = 0
 
 
 
 def Cap():
-    timer_analysis_start = time.time
-    capture = pyshark.LiveCapture("WI-FI", display_filter='tcp.analysis.fast_retransmission')
+    timer_analysis_start = time.time()
+    #print("Start time: " , timer_analysis_start)
+    capture = pyshark.LiveCapture("WI-FI")
 
     for packet in capture:
-        timer_analysis_end = time.time
+        timer_analysis_end = time.time()
+        #print("End time: ", timer_analysis_end)
         alarm = False
         counters_up_to_date(packet)
 
-        if timer_analysis_end - timer_analysis_start > 5 :      #Analysis every 5 seconds
-            timer_analysis_start = time.time
-            (alarm, attack) = analysis()
+        if timer_analysis_end - timer_analysis_start > 1 :      #Analysis every 5 seconds
+            timer_analysis_start = time.time()
+            (alarm, attack) = analysis(packet)
+            #print(alarm, attack)
 
         if alarm:
+            print(attack)
             distribute(packet)
-            mitigation(attack)
+            #mitigation(attack)
 
 
 def counters_up_to_date(packet) :
-    if "syn" in packet :
+    #print("In counter_up_to_date")
+    global SYN_counter
+    global ACK_counter
+    global UDP_counter
+    #This needs to change to flag_syn or whataver represents the syn flag
+    if "tcp" in packet :
         SYN_counter += 1
+    #This needs to change to flag_ack or whataver represents the ack flag
     elif "ack" in packet :
         ACK_counter += 1
+    elif "udp" in packet :
+        UDP_counter += 1
 
 
 def distribute(packet) :
-    TCP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/TCP.txt", "a")
-    UDP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP.txt", "a")
-    UDP_DNS_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP_DNS.txt", "a")
+    TCP_object = open(r"TCP.txt", "a")
+    UDP_object = open(r"UDP.txt", "a")
+    UDP_DNS_object = open(r"UDP_DNS.txt", "a")
+    #TCP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/TCP.txt", "a")
+    #UDP_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP.txt", "a")
+    #UDP_DNS_object = open(r"C:/Users/edina/Downloads/Lang. Based Sec/UDP_DNS.txt", "a")
     try:
         #counts_seq += 1
         protocol = packet.transport_layer
@@ -58,21 +72,22 @@ def distribute(packet) :
         #Check if TCP syn and fin messages are fine then scan trough the tcp and fetch to file
         field_names = packet.tcp._all_fields
         field_values = packet.tcp._all_fields.values()
-        for field_name in field_names:
-            for field_value in field_values:
-                if field_name == 'tcp.payload':
-                    print(f'{field_name} -- {field_value[100:120, 1]}') #100:120, 1
-                    print("*"*10 + "Continuiing" + "*"*10)
-
-        if "tcp" in packet and "ip" not in packet:
+        #for field_name in field_names:
+            #for field_value in field_values:
+                #if field_name == 'tcp.payload':
+                    #print(f'{field_name} -- {field_value[100:120, 1]}') #100:120, 1
+                    #print("*"*10 + "Continuiing" + "*"*10)
+        #We only want to save SYN packets here so check the SYN flag 
+        if "tcp" in packet:
             #packet_time = packet.sniff_time
             str_N = ('Name: ', packet.layers) #[-2]
             str_S_p = ('Scr. Port: ', packet[protocol].srcport)
             str_D_p = ('Dst. Port: ', packet[protocol].dstport)
             str1 = (str_N, str_S_p, str_D_p )# "Seq.": packet.sequence, "Len": packet.length, "Time": time.time, "Attri": TLP_attr   packet_time
+            print("Write to the tcp file")
             TCP_object.writelines(str(str1) + os.linesep)
             print ('%s  %s:%s --> %s:%s' % (str_N, packet[protocol].srcaddr, str_S_p, str_D_p, packet[protocol].dstaddr))
-            TCP_list.extend(packet)
+            #TCP_list.extend(packet)
         elif "udp" in packet:
             packet_time2 = packet.sniff_time
             str_N_2 = ('Name: ', packet.layers[-2])
@@ -80,8 +95,9 @@ def distribute(packet) :
             str_D_p_2 = ('Dst. Port: ', packet[protocol].dstport)
             str2 = (str_N_2, str_S_p_2, str_D_p_2, packet_time2)
             #{"Name":packet.layers, "Src. Port": packet[protocol].srcport, "Dst. Port": packet[protocol].dstport, "Len": packet.length, "Time": time.time}
+            print("Write to the udp file")
             UDP_object.writelines(str(str2) + os.linesep)
-            UDP_list.extend(packet)
+            #UDP_list.extend(packet)
             print ('%s  %s:%s --> %s:%s' % (str_N_2, packet[protocol].srcaddr, str_S_p, str_D_p, packet[protocol].dstaddr))
         
         # if "tcp" in packet or "udp" in packet :
@@ -97,7 +113,7 @@ def distribute(packet) :
             str3 += ('Scr. Port: ', packet[protocol].srcport)
             str3 += ('Dst. Port: ', packet[protocol].dstport)
             UDP_DNS_object.writelines(str(str3) + os.linesep)
-            UDP_list_DNS.extend(packet)
+            #UDP_list_DNS.extend(packet)
         #
         #
         #Here look for more packet petterns!
@@ -121,16 +137,28 @@ Attcak_list2 = []
 #ICMP FLood
 #Cases-switch here
 #Assume that syn flood is coming from one ip address, then its easier to block
-def analysis():
-    start_time = time.time
-    if SYN_counter - ACK_counter > 100 :
-        SYN_counter = 0
-        ACK_counter = 0
-        return (True, 'DoS')
-    else :
-        SYN_counter = 0
-        ACK_counter = 0
-    return(False, "")
+def analysis(packet):
+    global SYN_counter
+    global ACK_counter
+    global UDP_counter
+    start_time = time.time()
+    if "tcp" in packet:
+        if SYN_counter - ACK_counter > 10 :
+            SYN_counter = 0
+            ACK_counter = 0
+            return (True, 'TCP SYN Flood')
+        else :
+            SYN_counter = 0
+            ACK_counter = 0
+            return(False, "No TCP SYN flood")
+    elif "udp" in packet:
+        if UDP_counter > 10:
+            UDP_counter = 0
+            return (True, "UDP Flood")
+        else:
+            return (False, "No UDP flood")
+    else:
+        return (False, "No TCP or UDP")
  
 """
 Idea:
